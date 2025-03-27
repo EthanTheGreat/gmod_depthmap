0c46062c2d324c4c0e9198ccf2183b7fe20d455f
Ethan TheGreat <EthanTheGreatest@gmail.com>
2025-03-27 09:16:14 -0400

    Update cl_depthmap.lua

--- cl_depthmap.lua
+++ cl_depthmap.lua
@@ -1,15 +1,16 @@
 local dw = CreateMaterial("_DepthWrite"..math.random(1,1000000), "DepthWrite", {
     ["$no_fullbright"] = "1",
-    ["$color_depth"] = "1"
+    ["$color_depth"] = "1",
+    ["$model"] = "1",
 })
 
-local textureRT = GetRenderTargetEx("_rt_depthpro",
-    ScrW()/4, ScrH()/4, -- Downsample for higher FPS
+local textureRT = GetRenderTargetEx("_rt_depthpro", 
+    ScrW(), ScrH(), -- Downsample for higher FPS
     RT_SIZE_NO_CHANGE,
     MATERIAL_RT_DEPTH_SHARED,
     bit.bor(2, 256),
     0,
-    24 -- 1 - 8bit, 24 - 16bit, 29 32bit ** Restart Game **
+    1 -- 1 - 8bit, 24 - 16bit, 29 32bit ** Restart Game **
 )
 local renderMat = CreateMaterial("_rt_depthpro_mat"..math.random(1,1000000), "UnlitGeneric", {
     ["$basetexture"] = textureRT:GetName();
@@ -25,7 +26,9 @@ hook.Add( "RenderScene", "RenderDepthWrite", function()
     render.ModelMaterialOverride( dw )
 
         render.SetShadowColor(0,0,0)
-        render.RenderView( ) -- This can be edited to suit shadow depth
+
+        render.RenderView() -- This can be edited to suit shadow depth
+
         render.SetShadowColor(255,255,255)
 
     render.MaterialOverride(nil);
@@ -39,8 +42,8 @@ end )
  
 
 -- To Fix Skybox
-local min = Vector(1,1,1)*10000;
-local max = Vector(1,1,1)*10000;
+local min = Vector(1,1,1)*16000;
+local max = Vector(1,1,1)*16000;
 hook.Add("PostDraw2DSkyBox", "DepthWriteSkybox", function()
     local rt = render.GetRenderTarget()
     if !rt or rt:GetName() != "_rt_depthpro" then  
@@ -48,13 +51,9 @@ hook.Add("PostDraw2DSkyBox", "DepthWriteSkybox", function()
     end
     local cViewSetup = render.GetViewSetup();
 
-    cam.Start3D( zero_vec, cViewSetup.angles )
-    
-
     render.SetMaterial( dw )
-    
-    render.DrawBox( vector_origin, angle_zero, max, min, color_white )
-    cam.End3D()
+    render.SetColorMaterial()
+    render.DrawScreenQuad();
 end)
 
 -- To Debug Depth Buffer --
@@ -64,3 +63,6 @@ hook.Add("HUDPaint", "debug_view", function()
     surface.SetMaterial(renderMat)
     surface.DrawTexturedRect(0,0,ScrW()*scale,ScrH()*scale)
 end )
+
+-- Use: +mat_texture_list with top left checkbox marked, click RTs to see the textures at work.
+-- Improves depth calculations from 8 to 16 bit, doubling the precision. 
